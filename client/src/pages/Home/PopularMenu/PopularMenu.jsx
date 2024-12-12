9d5394d7ab56cfb64fd25634edfc7b0ee3e5992e
Arman-Kabir <armankabir8@gmail.com>
2024-12-12 15:37:04 +0600

    popularMenu

--- client/src/pages/Home/PopularMenu/PopularMenu.jsx
+++ client/src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -3,17 +3,29 @@ import SectionHeader from "../../../components/SectionHeader/SectionHeader";
 
 export default function PopularMenu() {
   const [menu, setMenu] = useState([]);
+
   useEffect(() => {
     fetch("menu.json")
       .then((res) => res.json())
-      .then((data) => 
-      {data.filter=>}
-        setMenu(data));
+      .then((data) => {
+        const popularMenu = data.filter((menu) => menu.category === "popular");
+        setMenu(popularMenu);
+      });
   }, []);
+
   console.log(menu);
+
   return (
     <section>
-      <SectionHeader heading="FROM OUR MENU" subHeading="Check it out"></SectionHeader>
+      <SectionHeader
+        heading="FROM OUR MENU"
+        subHeading="Check it out"
+      ></SectionHeader>
+      <div className="">
+        {
+          menu.map((menu)=>console.log(menu))
+        }
+      </div>
     </section>
   );
 }
