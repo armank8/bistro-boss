db7e4d31ce97b53caae6696450d4f8a5417b454a
Arman-Kabir <armankabir8@gmail.com>
2024-12-15 08:35:46 +0600

    Featured1

--- client/src/pages/Home/Featured/Featured.jsx
+++ client/src/pages/Home/Featured/Featured.jsx
@@ -3,13 +3,18 @@ import img from "../../../assets/home/featured.jpg";
 
 export default function Featured() {
   return (
-    <section className="bg-fixed w-full mx-0 h-[848px] bg-black pt-1 my-20" style={{ backgroundImage: `url(${img})` }}>
+    <section
+      className="bg-fixed w-full mx-0 h-[848px] bg-black pt-1 my-20"
+      style={{ backgroundImage: `url(${img})` }}
+    >
       <SectionHeader
         heading={"From our Menu"}
         subHeading={"Check it out"}
       ></SectionHeader>
       <div className="flex text-white px-40 py-20 space-x-10 bg-opacity-30">
-        <img className="w-[648px] h-[400px]" src={img} alt="" />
+        <div>
+          <img className="w-[648px] h-[400px]" src={img} alt="" />
+        </div>
         <div>
           <p>March 20, 2023</p>
           <p>WHERE CAN I GET SOME?</p>
