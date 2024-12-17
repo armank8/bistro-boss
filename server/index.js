6a18a3561efcf40b9f164c5cfc756837cf4388f6
Arman-Kabir <armankabir8@gmail.com>
2024-12-17 20:20:42 +0600

    menu api gateway

--- server/index.js
+++ server/index.js
@@ -25,6 +25,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const menuCollection = client.db("bistro-boss").collection("menu");
+
+    // Get Menu
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -32,7 +41,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
