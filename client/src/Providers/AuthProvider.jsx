42869423b5c532bef0e459879198371838162975
Arman-Kabir <armankabir8@gmail.com>
2024-12-23 15:45:43 +0600

    firebase AuthProvider setup

--- client/src/Providers/AuthProvider.jsx
+++ client/src/Providers/AuthProvider.jsx
@@ -3,14 +3,15 @@ import { getAuth } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
+
 const auth = getAuth(app);
 
-export default function AuthProvider() {
+export default function AuthProvider({children}) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const userInfo = {
     user,
     loading,
   };
-  return <AuthContext.Provider value={userInfo}></AuthContext.Provider>;
+  return <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>;
 }
