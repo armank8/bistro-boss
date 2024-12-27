c416cdc85e38ebefc10acf73f74510eab8aa9db7
Arman-Kabir <armankabir8@gmail.com>
2024-12-27 17:08:39 +0600

    PrivateRoute

--- client/src/Routes/PrivateRoute.jsx
+++ client/src/Routes/PrivateRoute.jsx
@@ -1,9 +1,10 @@
 import { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 export default function PrivateRoute({ children }) {
   const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
   if (loading) {
     return <progress className="progress w-56"></progress>;
   }
@@ -11,5 +12,5 @@ export default function PrivateRoute({ children }) {
     return children;
   }
 
-  return <Navigate to="/login"></Navigate>;
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 }
--- client/src/pages/Login/Login/Login.jsx
+++ client/src/pages/Login/Login/Login.jsx
@@ -11,13 +11,17 @@ import { AuthContext } from "../../../Providers/AuthProvider";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import Swal from "sweetalert2";
 
 export default function Login() {
   const [disabled, setDisabled] = useState(true);
   const { signIn } = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+  let from = location.state?.from?.pathname || "/";
+
   useEffect(() => {
     loadCaptchaEnginge(6);
   }, []);
@@ -41,6 +45,7 @@ export default function Login() {
           popup: ` animate__animated animate__fadeOutDown animate__faster `,
         },
       });
+      navigate(from, { replace: true });
     });
   };
 
