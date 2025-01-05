c57d8f14a8373b66debccba5e4c961b49d8670f9
Arman-Kabir <armankabir8@gmail.com>
2025-01-05 22:42:14 +0600

    jwt setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
