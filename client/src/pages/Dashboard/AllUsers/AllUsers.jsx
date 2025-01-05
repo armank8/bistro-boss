cbd8c194c7ae02854a7cbb7a1330db86248b71b0
Arman-Kabir <armankabir8@gmail.com>
2025-01-05 12:57:41 +0600

    All Users created

--- client/src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ client/src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,9 +1,66 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaTrashAlt, FaUsers } from "react-icons/fa";
+
 const AllUsers = () => {
+  const axiosSecure = useAxiosSecure();
+
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      return res.data;
+    },
+  });
+  //   console.log(users);
+
+  const handleDeleteUser = (user) => {};
   return (
     <div>
       <div className="flex justify-evenly my-4">
         <h2 className="text-3xl">AllUsers</h2>
-        <h2 className="text-3xl">Total Users</h2>
+        <h2 className="text-3xl">Total Users: {users.length}</h2>
+      </div>
+      {/* data table */}
+      <div className="overflow-x-auto">
+        <table className="table table-zebra w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <button
+                    onClick={() => handleDeleteUser(user)}
+                    className="btn  btn-md bg-orange-500"
+                  >
+                    <FaUsers className="text-white text-2xl"></FaUsers>
+                  </button>
+                </td>
+                {/* Delete a user */}
+                <td>
+                  <button
+                    onClick={() => handleDeleteUser(user)}
+                    className="btn btn-ghost btn-lg"
+                  >
+                    <FaTrashAlt className="text-red-600"></FaTrashAlt>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
