79ab3fcfa93ce2e9e2a3b2a4c15be415c7f29bb3
Arman-Kabir <armankabir8@gmail.com>
2025-01-30 23:45:19 +0600

    imgbb errors

--- client/src/pages/Dashboard/AddItems/AddItems.jsx
+++ client/src/pages/Dashboard/AddItems/AddItems.jsx
@@ -1,11 +1,29 @@
 import { useForm } from "react-hook-form"
 import SectionHeader from "../../../components/SectionHeader/SectionHeader"
 import { FaUtensils } from "react-icons/fa"
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
+
 
 const AddItems = () => {
-    const { register, handleSubmit } = useForm()
-    const onSubmit = (data) => {
-        console.log(data)
+    const { register, handleSubmit } = useForm();
+    const axiosPublic = useAxiosPublic();
+    console.log(image_hosting_key);
+
+    const onSubmit = async (data) => {
+        console.log(data);
+        // image upload to imgbb and then get an url
+        const imageFile = { image: data.image[0] };
+        const formData = new FormData();
+        formData.append('image', data.image[0]);
+        const res = await axiosPublic.post(image_hosting_api, formData, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        });
+        console.log(res.data);
     }
     return (
         <div>
@@ -27,8 +45,8 @@ const AddItems = () => {
                             <div className="label">
                                 <span className="label-text">Category*</span>
                             </div>
-                            <select  {...register("category", { required: true })} className="select select-bordered w-full">
-                                <option disabled>Select a category</option>
+                            <select defaultValue="default"  {...register("category", { required: true })} className="select select-bordered w-full">
+                                <option disabled value="default">Select a category</option>
                                 <option value="salad">Salad</option>
                                 <option value="pizza">Pizza</option>
                                 <option value="soup">Soup</option>
