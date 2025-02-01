a25106070343c1474cf41ad4585b04b0c326536f
Arman-Kabir <armankabir8@gmail.com>
2025-02-01 20:09:27 +0600

    payment

--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -16,6 +16,7 @@ import AddItems from "../pages/Dashboard/AddItems/AddItems";
 import AdminRoute from "./AdminRoute";
 import ManageItems from "../pages/Dashboard/ManageItems/ManageItems";
 import UpdateItem from "../pages/Dashboard/UpdateItem/UpdateItem";
+import Payment from "../pages/Dashboard/Payment/Payment";
 
 const router = createBrowserRouter([
   {
@@ -65,6 +66,10 @@ const router = createBrowserRouter([
         path: "cart",
         element: <Cart></Cart>,
       },
+      {
+        path: 'payment',
+        element: <Payment></Payment>
+      },
 
       // Admin only Routes
       {
@@ -78,7 +83,7 @@ const router = createBrowserRouter([
       {
         path: 'updateItem/:id',
         element: <AdminRoute> <UpdateItem></UpdateItem> </AdminRoute>,
-        loader: ({params}) => fetch(`http://localhost:5000/menu/${params.id}`)
+        loader: ({ params }) => fetch(`http://localhost:5000/menu/${params.id}`)
       },
       {
         path: 'users',
---
+++ client/src/pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,14 @@
+import SectionHeader from "../../../components/SectionHeader/SectionHeader"
+
+const Payment = () => {
+    return (
+        <div>
+            <SectionHeader heading="Payment" subHeading="Please pay to eat" className="pt-4"></SectionHeader>
+            <div>
+                <h2 className="text-4xl text-center">Payment</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Payment
