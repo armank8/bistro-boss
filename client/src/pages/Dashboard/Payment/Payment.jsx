921663d963480b1ac40ffe1f7984913d81cd7a85
Arman-Kabir <armankabir8@gmail.com>
2025-02-01 21:44:38 +0600

    stripe added and payment comp

--- client/src/pages/Dashboard/Payment/Payment.jsx
+++ client/src/pages/Dashboard/Payment/Payment.jsx
@@ -1,4 +1,9 @@
 import SectionHeader from "../../../components/SectionHeader/SectionHeader"
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+// TODO : Add publishable key
+const stripePromise = loadStripe('');
 
 const Payment = () => {
     return (
@@ -6,6 +11,9 @@ const Payment = () => {
             <SectionHeader heading="Payment" subHeading="Please pay to eat" className="pt-4"></SectionHeader>
             <div>
                 <h2 className="text-4xl text-center">Payment</h2>
+                <Elements stripe={stripePromise}>
+
+                </Elements>
             </div>
         </div>
     )
