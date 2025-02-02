04dc7c5a7908e550de0ffdf37affedb7430fce3b
Arman-Kabir <armankabir8@gmail.com>
2025-02-02 13:55:47 +0600

    paymnt intent

--- client/src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ client/src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,6 +1,8 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const CheckoutForm = () => {
+    const [error, setError] = useState(' ');
     const stripe = useStripe();
     const elements = useElements();
 
@@ -20,9 +22,11 @@ const CheckoutForm = () => {
         })
 
         if (error) {
-            console.log('payment error', error)
+            console.log('payment error', error);
+            setError(error.message);
         } else {
             console.log('payment method', paymentMethod);
+            setError(' ');
         }
 
     }
@@ -48,6 +52,7 @@ const CheckoutForm = () => {
                 <button className="btn btn-sm btn-primary my-4" type="submit" disabled={!stripe}>
                     Pay
                 </button>
+                <p className="text-red-600">{error}</p>
             </form>
         </div>
     )
--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ const cors = require("cors");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
 const app = express();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middlewares
@@ -211,6 +212,24 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
+
+    // stripe payment intent
+    app.post('create-payment-intent',async(req,res)=>{
+      const {price} = req.body;
+      const amount = parseInt(price*100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount:amount,
+        currency:'usd',
+        payment_method_types:['card']
+      });
+      res.send({
+        clientSecret:paymentIntent.client_secret
+      })
+
+
+    })
+
 
     // ---------------------------------------
     // Send a ping to confirm a successful connection
