4b2edf61b27e94545d3e3c6aa12c26b6b4634641
Arman-Kabir <armankabir8@gmail.com>
2025-02-21 08:44:06 +0600

    api race condition

--- client/src/Providers/AuthProvider.jsx
+++ client/src/Providers/AuthProvider.jsx
@@ -69,14 +69,16 @@ export default function AuthProvider({ children }) {
           // console.log(res.data.token);
           if (res.data.token) {
             localStorage.setItem("access-token", res.data.token);
+            setLoading(false);
           }
         });
       } else {
         // TODO:remove token(if token stored in the client side:Local storage,caching, in memory)
         localStorage.removeItem("access-token");
+        setLoading(false);
       }
 
-      setLoading(false);
+      // setLoading(false);
     });
     return () => {
       return unsubscribe();
